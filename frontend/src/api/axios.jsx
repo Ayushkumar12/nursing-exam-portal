646d8726aea0c73556a1f772238b7d22eae1f2a2
Ayushkumar12 <ayushkumarak112003@gmail.com>
2026-01-02 11:42:28 +0530

    ok

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://nursing-exam-portal.onrender.com/api',
+  baseURL: '/api',
 });
 
 api.interceptors.request.use((config) => {
